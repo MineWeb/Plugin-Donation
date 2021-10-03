fa928e360f6ec840e636adb30d545fca90874d9c
LaSkyMania <48296837+LaSkyMania@users.noreply.github.com>
2021-10-03 23:29:01 +0200

    Add files via upload

--- SQL/schema.php
+++ SQL/schema.php
@@ -1,29 +1,30 @@
 <?php
-class DonationAppSchema extends CakeSchema {
+class DonationAppSchema extends CakeSchema
+{
+	public $file = 'schema.php';
 
-    public $file = 'schema.php';
+	public function before($event = [])	{
+		return true;
+	}
 
-    public function before($event = array()) {
-        return true;
+	public function after($event = [])	{
     }
 
-    public function after($event = array()) {}
-
-    public $donation__donations = [
-        'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false, 'key' => 'primary'],
-        'objectif' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false],
-        'total' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false],
-        'email' => ['type' => 'string', 'null' => true, 'default' => null, 'length' => 255, 'unsigned' => false],
-        'description' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false]
+    public $donation__config = [
+        'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false, 'key' => 'primary'],
+        'objectif' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false],
+        'total' => ['type' => 'integer', 'null' => false, 'default' => 0, 'length' => 20, 'unsigned' => false],
+        'email' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
+        'description' => ['type' => 'string', 'null' => true, 'default' => null, 'length' => 255, 'unsigned' => false],
+        'color' => ['type' => 'string', 'null' => true, 'default' => '#3377ff', 'length' => 255, 'unsigned' => false]
     ];
 
-    public $donation__paypal_histories = [
-        'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false, 'key' => 'primary'],
-        'payment_id' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
-        'user_id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false],
-        'offer_id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'unsigned' => false],
-        'payment_amount' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
-        'item' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
-        'created' => ['type' => 'timestamp', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'],
+    public $donation__history = [
+        'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false, 'key' => 'primary'],
+        'payment_id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false],
+        'user_pseudo' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
+        'email' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 255, 'unsigned' => false],
+        'payment_amount' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false],
+        'created' => ['type' => 'datetime', 'null' => false, 'default' => null]
     ];
 }

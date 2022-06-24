69748400e6167b61e259d54916ef548f58bbed14
LaSkyMania <48296837+LaSkyMania@users.noreply.github.com>
2022-06-24 21:50:14 +0200

    Update DonationConfig.php

--- Model/DonationConfig.php
+++ Model/DonationConfig.php
@@ -4,19 +4,21 @@ class DonationConfig extends DonationAppModel
 {
     public $useTable = "config";
 
-    public function add($objectif, $email, $description, $color)
+    public function add($objectif, $email, $description, $color, $command, $serverid)
     {
         $this->create();
-        $this->set(array(
+        $this->set([
             'objectif' => $objectif,
             'email' => $email,
             'description' => $description,
-            'color' => $color
-        ));
+            'color' => $color,
+            'command' => $command,
+            'server_id' => $serverid
+        ]);
         $this->save();
     }
 
-    public function edit($objectif, $email, $description, $color)
+    public function edit($objectif, $email, $description, $color, $command, $serverid)
     {
         $find = $this->find('first');
         if(empty($find))
@@ -25,12 +27,16 @@ class DonationConfig extends DonationAppModel
         $email = $this->getDataSource()->value($email, 'string');
         $description = $this->getDataSource()->value($description, 'string');
         $color = $this->getDataSource()->value($color, 'string');
+        $command = $this->getDataSource()->value($command, 'string');
+        $serverid = $this->getDataSource()->value($serverid, 'int');
 
         return $this->updateAll([
             'objectif' => $objectif,
             'email' => $email,
             'description' => $description,
-            'color' => $color
+            'color' => $color,
+            'command' => $command,
+            'server_id' => $serverid
         ], ['id' => 1]);
     }
 
